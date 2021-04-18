1c06e2a273478ba91e8346231267098c92a86f49
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-17 19:24:40 -0600

    added findall findbyid and deletebyid to clubimpl

--- src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
@@ -1,11 +1,13 @@
 package com.lambdaschool.oktafoundation.services;
 
+import com.lambdaschool.oktafoundation.exceptions.ResourceNotFoundException;
 import com.lambdaschool.oktafoundation.models.Club;
 import com.lambdaschool.oktafoundation.repository.ClubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
@@ -17,12 +19,20 @@ public class ClubServiceImpl implements ClubService{
 
     @Override
     public List<Club> findAll() {
-        return null;
+        List<Club> clubList = new ArrayList<>();
+
+        clubrepos.findAll()
+                .iterator()
+                .forEachRemaining(clubList::add);
+        return clubList;
     }
 
     @Override
-    public Club findClubById(Long clubid) {
-        return null;
+    public Club findClubById(Long clubid) throws
+            ResourceNotFoundException
+    {
+        return clubrepos.findById(clubid)
+            .orElseThrow(() -> new ResourceNotFoundException("Club id" + clubid + "not found!"));
     }
 
     @Override
@@ -37,6 +47,8 @@ public class ClubServiceImpl implements ClubService{
 
     @Override
     public void delete(long clubid) {
-
+        clubrepos.findById(clubid)
+                .orElseThrow(() -> new ResourceNotFoundException("Club id" + clubid + "not found!"));
+        clubrepos.deleteById(clubid);
     }
 }

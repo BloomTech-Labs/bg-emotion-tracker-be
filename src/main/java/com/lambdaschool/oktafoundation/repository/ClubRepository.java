2792f563ca67d617d0b10bddce9505f812153a12
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-17 19:18:16 -0600

    added club repository and imported methods from clubservice impls

---
+++ src/main/java/com/lambdaschool/oktafoundation/repository/ClubRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.oktafoundation.repository;
+
+import com.lambdaschool.oktafoundation.models.Club;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClubRepository extends CrudRepository<Club, Long> {
+
+}
--- src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
@@ -1,4 +1,42 @@
 package com.lambdaschool.oktafoundation.services;
 
-public class ClubServiceImpl {
+import com.lambdaschool.oktafoundation.models.Club;
+import com.lambdaschool.oktafoundation.repository.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service(value = "clubService")
+public class ClubServiceImpl implements ClubService{
+
+    @Autowired
+    private ClubRepository clubrepos;
+
+    @Override
+    public List<Club> findAll() {
+        return null;
+    }
+
+    @Override
+    public Club findClubById(Long clubid) {
+        return null;
+    }
+
+    @Override
+    public Club save(Club newClub) {
+        return null;
+    }
+
+    @Override
+    public void update(Club updateClub, long clubid) {
+
+    }
+
+    @Override
+    public void delete(long clubid) {
+
+    }
 }

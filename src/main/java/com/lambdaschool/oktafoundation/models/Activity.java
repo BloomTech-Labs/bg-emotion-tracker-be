4211f5f2b83b8c5f8eb8a831a7ec7c6a507eb143
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-04-21 16:36:10 -0600

    employee model annotated

--- src/main/java/com/lambdaschool/oktafoundation/models/Activity.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/Activity.java
@@ -1,18 +1,28 @@
 package com.lambdaschool.oktafoundation.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@ApiModel(value = "Activities", description = "Activities available")
 @Entity
 @Table(name = "activities")
 public class Activity extends Auditable
 {
+
+    @ApiModelProperty(name = "Activity Id",
+            value = "Primary Key for Activity",
+            required = true,
+            example = "1"
+    )
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long activityid;
+
 
     @OneToMany(mappedBy = "activity",
             cascade = CascadeType.ALL,
@@ -20,6 +30,11 @@ public class Activity extends Auditable
     @JsonIgnoreProperties(value = "activity", allowSetters = true)
     private Set<ClubActivities> clubs = new HashSet<>();
 
+    @ApiModelProperty(name = "Activity Name",
+            value = "Full activity name",
+            required = true,
+            example = "Piano Lessons"
+    )
 
     @Column(nullable = false)
     private String activityname;

6d63b8296bbf4daf261d9fcd19944cd5d1546c61
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-04-22 15:04:09 -0600

    clubs/activities and join table seed data

--- src/main/java/com/lambdaschool/oktafoundation/SeedData.java
+++ src/main/java/com/lambdaschool/oktafoundation/SeedData.java
@@ -99,38 +99,119 @@ public class SeedData
         Activity a7 = new Activity("Soccer");
 
 //        Club
+        //Anderson has Attendance, Arts, Archery, Basketball, Homework
         Club c1 = new Club("Anderson");
         c1.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c1,a1));
+        c1.getActivities()
+                .add(new ClubActivities(c1,a2));
+        c1.getActivities()
+                .add(new ClubActivities(c1,a3));
+        c1.getActivities()
+                .add(new ClubActivities(c1,a4));
+        c1.getActivities()
+                .add(new ClubActivities(c1,a5));
 
+        //Caitlin has Attendance,Arts, Basketball, Homework,
         Club c2 = new Club("Caitlin");
         c2.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c2, a1));
+        c2.getActivities()
+                .add(new ClubActivities(c2, a2));
+        c2.getActivities()
+                .add(new ClubActivities(c2, a4));
+        c2.getActivities()
+                .add(new ClubActivities(c2, a5));
 
+        //Grossman has Attendance, Arts, Basketball, Homework, Music, Soccer
         Club c3 = new Club("Grossman");
         c3.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c3, a1));
+        c3.getActivities()
+                .add(new ClubActivities(c3, a2));
+        c3.getActivities()
+                .add(new ClubActivities(c3, a4));
+        c3.getActivities()
+                .add(new ClubActivities(c3, a5));
+        c3.getActivities()
+                .add(new ClubActivities(c3, a6));
+        c3.getActivities()
+                .add(new ClubActivities(c3, a7));
 
+        //Johnston has Attendance, Arts, Basketball, Homework, Music, Soccer
         Club c4 = new Club("Johnston");
         c4.getActivities()
-            .add(new ClubActivities());
+                .add(new ClubActivities(c4, a1));
+        c4.getActivities()
+                .add(new ClubActivities(c4, a2));
+        c4.getActivities()
+                .add(new ClubActivities(c4, a4));
+        c4.getActivities()
+                .add(new ClubActivities(c4, a5));
+        c4.getActivities()
+                .add(new ClubActivities(c4, a6));
+        c4.getActivities()
+                .add(new ClubActivities(c4, a7));
 
+        //Marley has Attendance, Arts, Basketball, Homework Help, Music, Soccer
         Club c5 = new Club("Marley");
         c5.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c5, a1));
+        c5.getActivities()
+                .add(new ClubActivities(c5, a2));
+        c5.getActivities()
+                .add(new ClubActivities(c5, a4));
+        c5.getActivities()
+                .add(new ClubActivities(c5, a5));
+        c5.getActivities()
+                .add(new ClubActivities(c5, a6));
+        c5.getActivities()
+                .add(new ClubActivities(c5, a7));
 
+        //Morton has Attendance, Arts, Basketball, Homework, Soccer
         Club c6 = new Club ("Morton");
         c6.getActivities()
-                .add(new ClubActivities());
-
+                .add(new ClubActivities(c6, a1));
+        c6.getActivities()
+                .add(new ClubActivities(c6, a2));
+        c6.getActivities()
+                .add(new ClubActivities(c6, a4));
+        c6.getActivities()
+                .add(new ClubActivities(c6, a5));
+        c6.getActivities()
+                .add(new ClubActivities(c6, a7));
+        //Notter has Attendance, Arts, Basketball, Homework,
         Club c7 = new Club("Notter");
         c7.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c7, a1));
+        c7.getActivities()
+                .add(new ClubActivities(c7, a2));
+        c7.getActivities()
+                .add(new ClubActivities(c7, a4));
+        c7.getActivities()
+                .add(new ClubActivities(c7, a5));
 
+        //Stelle has Attendance, Arts
         Club c8 = new Club("Stelle");
         c8.getActivities()
-                .add(new ClubActivities());
+                .add(new ClubActivities(c8, a1));
+        c8.getActivities()
+                .add(new ClubActivities(c8, a2));
 
+        //Jefferson has Attendance, Arts, Basketball, Homework, Music, Soccer
+        Club c9 = new Club("Jefferson");
+        c9.getActivities()
+                .add(new ClubActivities(c9, a1));
+        c9.getActivities()
+                .add(new ClubActivities(c9, a2));
+        c9.getActivities()
+                .add(new ClubActivities(c9, a4));
+        c9.getActivities()
+                .add(new ClubActivities(c9, a5));
+        c9.getActivities()
+                .add(new ClubActivities(c9, a6));
+        c9.getActivities()
+                .add(new ClubActivities(c9, a7));
 
         // The following is an example user!
         /*

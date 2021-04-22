280b023ab6d75f00224bb9c8ec78d184627b20a0
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-22 12:15:37 -0600

    created acitivity get post patch and delete by id methods

--- src/main/java/com/lambdaschool/oktafoundation/controllers/ActivityController.java
+++ src/main/java/com/lambdaschool/oktafoundation/controllers/ActivityController.java
@@ -3,13 +3,15 @@ package com.lambdaschool.oktafoundation.controllers;
 import com.lambdaschool.oktafoundation.models.Activity;
 import com.lambdaschool.oktafoundation.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,41 @@ public class ActivityController {
     {
         Activity a = activityService.findActivityById(activityid);
         return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/activity",
+    consumes = "application/json")
+    public ResponseEntity<?> addNewActivity(
+            @Valid @RequestBody Activity newActivity) throws URISyntaxException
+    {
+        newActivity.setActivityid(0);
+        newActivity = activityService.save(newActivity);
+
+//        Location Header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newActivityURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{userid}")
+                .buildAndExpand(newActivity.getActivityid())
+                .toUri();
+        responseHeaders.setLocation(newActivityURI);
+
+        return new ResponseEntity<>(null,
+                responseHeaders,
+                HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/activity/{activityid}",
+    consumes = "application/json")
+    public ResponseEntity<?> updateActivity(@RequestBody Activity updateActivity, @PathVariable long activityid)
+    {
+        activityService.update(updateActivity, activityid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/activity/{activityid}")
+    public ResponseEntity<?> deleteActivityById(@PathVariable long activityid)
+    {
+        activityService.delete(activityid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }

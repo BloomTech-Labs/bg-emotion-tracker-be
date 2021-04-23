7f16c3773c6ec1281c01f264c93308e067d08d68
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-23 07:57:03 -0600

    clubactivitiesid model documentation finished

--- src/main/java/com/lambdaschool/oktafoundation/models/ClubActivitiesId.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/ClubActivitiesId.java
@@ -3,11 +3,23 @@ package com.lambdaschool.oktafoundation.models;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+/**
+ * Class to represent the complex primary key for ClubActivities
+ */
 @Embeddable
 public class ClubActivitiesId implements Serializable {
+    /**
+     * The id of the club
+     */
     private long club;
+    /**
+     * The id of the activity
+     */
     private long activity;
 
+    /**
+     * The default constructor required by JPA
+     */
     public ClubActivitiesId() {
     }
 
@@ -15,19 +27,35 @@ public class ClubActivitiesId implements Serializable {
         this.club = user;
         this.activity = role;
     }
-
+    /**
+     * Getter for the club id
+     *
+     * @return long the club id
+     */
     public long getClub() {
         return club;
     }
-
+    /**
+     * Setter for the club id
+     *
+     * @return long the club id
+     */
     public void setClub(long club) {
         this.club = club;
     }
-
+    /**
+     * Getter for the activity id
+     *
+     * @return long the activity id
+     */
     public long getActivity() {
         return activity;
     }
-
+    /**
+     * Setter for the activity id
+     *
+     * @return long the activity id
+     */
     public void setActivity(long activity) {
         this.activity = activity;
     }

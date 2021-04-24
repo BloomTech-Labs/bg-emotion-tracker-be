d1446c96b550a56e7066a102785c90e36c9c3b71
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-24 16:41:59 -0600

    memberreactionsid documentation finished;

--- src/main/java/com/lambdaschool/oktafoundation/models/MemberReactionsId.java
+++ src/main/java/com/lambdaschool/oktafoundation/models/MemberReactionsId.java
@@ -3,11 +3,23 @@ package com.lambdaschool.oktafoundation.models;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+/**
+ * Class to represent the complex primary key for MemberReactions
+ */
 @Embeddable
 public class MemberReactionsId implements Serializable {
+    /**
+     * The id of the member
+     */
     private long member;
+    /**
+     * The id of the reaction
+     */
     private long reaction;
 
+    /**
+     * The default constructor required by JPA
+     */
     public MemberReactionsId() {
     }
 
@@ -15,24 +27,40 @@ public class MemberReactionsId implements Serializable {
         this.member = member;
         this.reaction = reaction;
     }
-
+    /**
+     * Getter for the member id
+     *
+     * @return long the member id
+     */
     public long getMember() {
         return member;
     }
-
+    /**
+     * Setter for the member id
+     *
+     * @return long the member id
+     */
     public void setMember(long member) {
         this.member = member;
     }
+    /**
+     * Getter for the reaction id
+     *
+     * @return long the reaction id
+     */
 
     public long getReaction() {
         return reaction;
     }
-
+    /**
+     * Setter for the reaction id
+     *
+     * @return long the reaction id
+     */
     public void setReaction(long reaction) {
         this.reaction = reaction;
     }
-
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

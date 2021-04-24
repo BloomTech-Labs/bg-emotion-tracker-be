f5f3f0fb2415f1a431b035814e9efd824309d633
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-24 17:43:08 -0600

    all Repositories documentation is finished

--- src/main/java/com/lambdaschool/oktafoundation/repository/ClubActivityRepository.java
+++ src/main/java/com/lambdaschool/oktafoundation/repository/ClubActivityRepository.java
@@ -2,6 +2,8 @@ package com.lambdaschool.oktafoundation.repository;
 
 import com.lambdaschool.oktafoundation.models.ClubActivities;
 import org.springframework.data.repository.CrudRepository;
-
+/**
+ * The CRUD repository connecting ClubActivity to the rest of the application
+ */
 public interface ClubActivityRepository extends CrudRepository<ClubActivities, Long> {
 }

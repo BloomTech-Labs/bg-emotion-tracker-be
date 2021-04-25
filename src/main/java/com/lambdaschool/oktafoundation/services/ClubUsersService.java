9e9183d2874e716992278042a90f3e646fc5d647
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2021-04-24 18:20:48 -0600

    services for activity clubactivity clubusers and memberreactions documentation finished

--- src/main/java/com/lambdaschool/oktafoundation/services/ClubUsersService.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubUsersService.java
@@ -3,9 +3,21 @@ package com.lambdaschool.oktafoundation.services;
 import com.lambdaschool.oktafoundation.models.ClubUsers;
 
 import java.util.List;
-
+/**
+ * The Service that works with ClubUsers Model.
+ */
 public interface ClubUsersService {
+    /**
+     * Returns a list of all the ClubUsers
+     *
+     * @return List of ClubUsers. If no clubusers, empty list.
+     */
     List<ClubUsers> findAll();
-
+    /**
+     * Returns the ClubUser with the given primary key.
+     *
+     * @param clubuserid The primary key (long) of the clubuser you seek.
+     * @return The given ClubUser or throws an exception if not found.
+     */
     ClubUsers findClubUserById(Long clubuserid);
 }
--- src/main/java/com/lambdaschool/oktafoundation/services/MemberReactionService.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/MemberReactionService.java
@@ -3,10 +3,21 @@ package com.lambdaschool.oktafoundation.services;
 import com.lambdaschool.oktafoundation.models.MemberReactions;
 
 import java.util.List;
-
+/**
+ * The Service that works with MemberReaction Model.
+ */
 public interface MemberReactionService {
-
+    /**
+     * Returns a list of all the MemberReactions
+     *
+     * @return List of MemberReactions. If no memberreactions, empty list.
+     */
     List<MemberReactions> findAll();
-
+    /**
+     * Returns the MemberReaction with the given primary key.
+     *
+     * @param id The primary key (long) of the memberreaction you seek.
+     * @return The given MemberReaction or throws an exception if not found.
+     */
     MemberReactions findMemberReactionById(Long id);
 }

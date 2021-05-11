3d00db2ab236335c5df93435747aa80efabf5442
SamuelPerrin <samwperrin@yahoo.com>
2021-05-11 11:20:03 -0500

    adds ReactionRepository

---
+++ src/main/java/com/lambdaschool/oktafoundation/repository/ReactionRepository.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.oktafoundation.repository;
+
+import com.lambdaschool.oktafoundation.models.Reaction;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReactionRepository extends CrudRepository<Reaction, Long> {
+}

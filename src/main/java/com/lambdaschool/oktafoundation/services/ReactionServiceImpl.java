8be44cb8c8dd66967cedd98bde1416e575b2097d
SamuelPerrin <samwperrin@yahoo.com>
2021-05-11 16:57:25 -0500

    adds ReactionServiceImpl

---
+++ src/main/java/com/lambdaschool/oktafoundation/services/ReactionServiceImpl.java
@@ -0,0 +1,62 @@
+package com.lambdaschool.oktafoundation.services;
+
+import com.lambdaschool.oktafoundation.exceptions.ResourceNotFoundException;
+import com.lambdaschool.oktafoundation.models.Member;
+import com.lambdaschool.oktafoundation.models.MemberReactions;
+import com.lambdaschool.oktafoundation.models.Reaction;
+import com.lambdaschool.oktafoundation.repository.ReactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@Service(value = "reactionService")
+public class ReactionServiceImpl implements ReactionService {
+
+    /**
+     * Connects this service to the Reaction table.
+     */
+    @Autowired
+    private ReactionRepository reactionrepos;
+
+    @Autowired
+    private MemberService memberService;
+
+    public Reaction findReactionById(long id) throws ResourceNotFoundException {
+        return reactionrepos.findById(id).orElseThrow(() -> new ResourceNotFoundException("Reaction id " + id + " not found!"));
+    }
+
+    @Override
+    public List<Reaction> findAll() {
+        List<Reaction> list = new ArrayList<>();
+        reactionrepos.findAll()
+                .iterator()
+                .forEachRemaining(list::add);
+        return list;
+    }
+
+    @Transactional
+    @Override
+    public Reaction save(Reaction reaction) {
+        Reaction newReaction = new Reaction();
+
+        if (reaction.getReactionid() != 0) {
+            reactionrepos.findById(reaction.getReactionid())
+                    .orElseThrow(() -> new ResourceNotFoundException("Reaction id " + reaction.getReactionid() + " not found!"));
+            newReaction.setReactionid(reaction.getReactionid());
+        }
+
+        newReaction.setReactionvalue(reaction.getReactionvalue());
+
+        newReaction.getMember().clear();
+        for (MemberReactions mr : reaction.getMember()) {
+            Member addMember = memberService.findMemberById(mr.getMember().getMember_table_id());
+            newReaction.getMember().add(new MemberReactions(addMember, newReaction, mr.getCheckedin(), mr.getClubactivity()));
+        }
+
+        return reactionrepos.save(newReaction);
+    }
+}

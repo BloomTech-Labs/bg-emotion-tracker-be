c92709e3dc336aa3a0500295ed0cd1fd91d3da72
obtl <abc1929@gmail.com>
2021-05-18 02:41:37 +0800

    added seeddata for club checkout; modified configs to allow main branch to be deployable to heroku

--- src/main/java/com/lambdaschool/oktafoundation/OktaFoundationApplication.java
+++ src/main/java/com/lambdaschool/oktafoundation/OktaFoundationApplication.java
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @EnableJpaAuditing
 @SpringBootApplication
-//@PropertySource("file:/E:/Coding/configs/oktafoundationconf.properties")
 public class OktaFoundationApplication
 {
     /**

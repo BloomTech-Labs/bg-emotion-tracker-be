5364f362f41312361b95d779e669928ae9c8cdc4
SamuelPerrin <samwperrin@yahoo.com>
2021-05-18 14:01:20 -0500

    updates and standardizes doc comments in User- and RoleRepository

--- src/main/java/com/lambdaschool/oktafoundation/repository/RoleRepository.java
+++ src/main/java/com/lambdaschool/oktafoundation/repository/RoleRepository.java
@@ -13,10 +13,10 @@ public interface RoleRepository
     extends CrudRepository<Role, Long>
 {
     /**
-     * JPA Query to find a role by name case insensitive search
+     * JPA Query to find a role by name case insensitive search.
      *
-     * @param name the name of the role which you seek
-     * @return the first role matching the given name using a case insensitive search
+     * @param name The name of the role which you seek
+     * @return The first role matching the given name using a case insensitive search
      */
     Role findByNameIgnoreCase(String name);
 

4d23294bef03ac6c8c9f80e87a0968668b2824ec
SamuelPerrin <samwperrin@yahoo.com>
2021-05-18 14:01:54 -0500

    updates doc comments in ClubRepository

--- src/main/java/com/lambdaschool/oktafoundation/repository/ClubRepository.java
+++ src/main/java/com/lambdaschool/oktafoundation/repository/ClubRepository.java
@@ -9,14 +9,18 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The CRUD repository connecting Club to the rest of the application
+ */
 public interface ClubRepository extends CrudRepository<Club, Long> {
-
-
+   /**
+    * JPA Query to get a summary of the Clubs containing the name and id of each Club
+    *
+    * @return The summary of all the Clubs
+    */
    @Query(
            value = "select clubid,clubname from clubs",
            nativeQuery = true
    )
    List<ClubSummary> getClubsSummary();
-
-
 }
